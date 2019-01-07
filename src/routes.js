529fb0b227abc0a27fac138398aa3a765e46647f
Musbahniar-MNR <ruang.9campus@gmail.com>
2019-01-07 10:21:27 +0700

    monday blue

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {BrowserRouter,  Route,  Switch} from 'react-router-dom';
 
 import Welcome from '././components/Welcome/Welcome';
@@ -21,27 +21,55 @@ import Kontak from '././components/Kontak/Kontak';
 
 import NotFound from '././components/NotFound/NotFound';
 
-const Routes = () => (
-  <BrowserRouter >
-      <Switch>
-          <Route exact path="/" component={Welcome} />
-          <Route path="/home" component={Home}/>
-          <Route path="/info" component={Info} />
-          <Route path="/kontak" component={Kontak} />
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={Signup} />
-          <Route path="/vak" component={Vak} />
-          <Route path="/cbt" component={Cbt} />
-          <Route path="/tocbt" component={ToCbt} />
-          <Route path="/tryout/:kodesoal" component={() => <Tryout />} />
-          <Route path="/dcbt/:pola/:periode" component={Cbtdetail} />
-          <Route path="/kuis" component={Kuis} />
-          <Route path="/presensi" component={Presensi} />
-          <Route path="/tutor" component={Vtutor} />
-          <Route path="*" component={NotFound}/>
-      </Switch>
 
-  </BrowserRouter>
-);
+export default class Routes extends Component {
 
-export default Routes;
+    render() {
+    
+        return (
+        <div>
+            <BrowserRouter >
+            <Switch>
+                <Route exact path="/" component={Welcome} />
+                <Route path="/home" component={Home}/>
+                <Route path="/info" component={Info} />
+                <Route path="/kontak" component={Kontak} />
+                <Route path="/login" component={Login} />
+                <Route path="/signup" component={Signup} />
+                <Route path="/vak" component={Vak} />
+                <Route path="/cbt" component={Cbt} />
+                <Route path="/tocbt" component={ToCbt} />
+                <Route path="/tryout/:kodesoal" component={Tryout} />
+                <Route path="/dcbt/:pola/:periode" component={Cbtdetail} />
+                <Route path="/kuis" component={Kuis} />
+                <Route path="/presensi" component={Presensi} />
+                <Route path="/tutor" component={Vtutor} />
+                <Route path="*" component={NotFound}/>
+            </Switch>
+            </BrowserRouter>
+        </div>
+        )
+    }
+}
+// const Routes = () => (
+//   <BrowserRouter >
+//       <Switch>
+//           <Route exact path="/" component={Welcome} />
+//           <Route path="/home" component={Home}/>
+//           <Route path="/info" component={Info} />
+//           <Route path="/kontak" component={Kontak} />
+//           <Route path="/login" component={Login} />
+//           <Route path="/signup" component={Signup} />
+//           <Route path="/vak" component={Vak} />
+//           <Route path="/cbt" component={Cbt} />
+//           <Route path="/tocbt" component={ToCbt} />
+//           <Route path="/tryout/:kodesoal" component={() => <Tryout />} />
+//           <Route path="/dcbt/:pola/:periode" component={Cbtdetail} />
+//           <Route path="/kuis" component={Kuis} />
+//           <Route path="/presensi" component={Presensi} />
+//           <Route path="/tutor" component={Vtutor} />
+//           <Route path="*" component={NotFound}/>
+//       </Switch>
+
+//   </BrowserRouter>
+// );
