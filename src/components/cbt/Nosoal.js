5cdb031eb74620227b442643a0a3d78cb41ebbc3
Musbahniar-MNR <ruang.9campus@gmail.com>
2019-01-07 12:05:38 +0700

    istirahat dulu

--- src/components/cbt/Nosoal.js
+++ src/components/cbt/Nosoal.js
@@ -1,14 +1,40 @@
-import React from 'react'
 import { Button, Table } from 'reactstrap';
+import React, { Component } from 'react'
 
-function Nosoal() {
+export default class Nosoal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input:'',
+            recipeList :
+            [{
+                recipe: "Tacos",
+                directions: "make tacos",
+                ingredients: ["meat"]
+            },
+            {
+                recipe: "pizza",
+                directions: "bake",
+                ingredients: ["dough"]
+            }]
+        };
+        // this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit() {
+        console.log('clickity');
+    }
+    
+    render() {
+        const {input} = this.state;
     return (
-        <div>
-            <h2>x</h2>
+      <div>
+          <h2>x</h2>
             <Table size="sm">
                 <tbody>
                 <tr>
-                    <th>1 A</th>
+                    <th> <Button onClick={this.handleSubmit}>Submit</Button></th>
                     <th>2 A</th>
                     <th>3 A</th>
                     <th>4 A</th>
@@ -31,8 +57,7 @@ function Nosoal() {
                         </tbody>
             </Table> 
             <Button color="primary">---SELESAI---</Button>{' '}
-        </div>
+      </div>
     )
+  }
 }
-
-export  default Nosoal
